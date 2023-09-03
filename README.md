d2cb2ea3f81731c8d635543559d7031cc449b1e4
d1r0l <xd1r0lx@gmail.com>
2023-09-03 23:47:07 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This repository contains results of exercises of part 9 of Full Stack Open 2023 performed by me
+
+## Folder "excercises" contains exercises 9.1=9.x
---
+++ excercises/bmiCalculator.ts
@@ -0,0 +1,54 @@
+interface calculateBmi {
+  weight: number;
+  height: number;
+}
+
+const parseArguments = (args: string[]): calculateBmi => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      weight: Number(args[2]),
+      height: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+const bmiCalculator = (weight: number, height: number) => {
+  const heightInMeters = height / 100;
+  const bmi = weight / Math.pow(2, heightInMeters);
+  switch (true) {
+    case bmi < 16:
+      return console.log('Underweight (Severe thinness)');
+    case bmi >= 16 && bmi < 17:
+      return console.log('Underweight (Moderate thinness)');
+    case bmi >= 17 && bmi < 18.5:
+      return console.log('Underweight (Mild thinness)');
+    case bmi >= 18.5 && bmi < 25:
+      return console.log('Normal (Healthy weight)');
+    case bmi >= 25 && bmi < 30:
+      return console.log('Overweight (Pre-Obese)');
+    case bmi >= 30 && bmi < 35:
+      return console.log('Obese (Class I)');
+    case bmi >= 35 && bmi < 40:
+      return console.log('Obese (Class II)');
+    case bmi >= 40:
+      return console.log('Obese (Class III)');
+    default:
+      throw new Error('bmi calculation failed');
+  }
+};
+
+try {
+  const parsedArgs = parseArguments(process.argv);
+  bmiCalculator(parsedArgs.weight, parsedArgs.height);
+} catch (error) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
