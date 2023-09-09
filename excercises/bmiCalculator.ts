f017e2040261c81280df2c38d838e988ba7d44b3
d1r0l <xd1r0lx@gmail.com>
2023-09-10 01:00:30 +0400

    completed ex 9.5

--- excercises/bmiCalculator.ts
+++ excercises/bmiCalculator.ts
@@ -3,52 +3,64 @@ interface calculateBmi {
   weight: number;
 }
 
-const parseArgumentsBmiCalculator = (args: string[]): calculateBmi => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+const parseArguments = (args: any): calculateBmi => {
+  if (!args.weight && !args.height)
+    throw new Error('malformatted parameters: weight & height is required');
+  if (!args.height)
+    throw new Error('malformatted parameters: height is required');
+  if (!args.weight)
+    throw new Error('malformatted parameters: weight is required');
+  if (args.weight <= 0)
+    throw new Error(
+      'malformatted parameters: weight cannot be zero or negative',
+    );
+  if (args.height <= 0)
+    throw new Error(
+      'malformatted parameters: height cannot be zero or negative',
+    );
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+  if (!isNaN(Number(args.height)) && !isNaN(Number(args.weight))) {
     return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
+      height: Number(args.height),
+      weight: Number(args.weight),
     };
   } else {
     throw new Error('Provided values were not numbers!');
   }
 };
 
-const bmiCalculator = (height: number, weight: number) => {
-  const heightInMeters = height / 100;
-  const bmi = weight / Math.pow(2, heightInMeters);
-  switch (true) {
-    case bmi < 16:
-      return console.log('Underweight (Severe thinness)');
-    case bmi >= 16 && bmi < 17:
-      return console.log('Underweight (Moderate thinness)');
-    case bmi >= 17 && bmi < 18.5:
-      return console.log('Underweight (Mild thinness)');
-    case bmi >= 18.5 && bmi < 25:
-      return console.log('Normal (Healthy weight)');
-    case bmi >= 25 && bmi < 30:
-      return console.log('Overweight (Pre-Obese)');
-    case bmi >= 30 && bmi < 35:
-      return console.log('Obese (Class I)');
-    case bmi >= 35 && bmi < 40:
-      return console.log('Obese (Class II)');
-    case bmi >= 40:
-      return console.log('Obese (Class III)');
-    default:
-      throw new Error('bmi calculation failed');
+const calculator = (args: calculateBmi): string => {
+  try {
+    const { height, weight } = args;
+    const heightInMeters = height / 100;
+    const bmi = weight / Math.pow(heightInMeters, 2);
+    switch (true) {
+      case bmi < 16:
+        return 'Underweight (Severe thinness)';
+      case bmi >= 16 && bmi < 17:
+        return 'Underweight (Moderate thinness)';
+      case bmi >= 17 && bmi < 18.5:
+        return 'Underweight (Mild thinness)';
+      case bmi >= 18.5 && bmi < 25:
+        return 'Normal (Healthy weight)';
+      case bmi >= 25 && bmi < 30:
+        return 'Overweight (Pre-Obese)';
+      case bmi >= 30 && bmi < 35:
+        return 'Obese (Class I)';
+      case bmi >= 35 && bmi < 40:
+        return 'Obese (Class II)';
+      case bmi >= 40:
+        return 'Obese (Class III)';
+      default:
+        throw new Error('bmi calculation failed');
+    }
+  } catch (error) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    return errorMessage;
   }
 };
 
-try {
-  const parsedArgs = parseArgumentsBmiCalculator(process.argv);
-  bmiCalculator(parsedArgs.height, parsedArgs.weight);
-} catch (error) {
-  let errorMessage = 'Something bad happened.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
+export default { parseArguments, calculator };
--- excercises/index.ts
+++ excercises/index.ts
@@ -1,10 +1,21 @@
 import express = require('express');
+import bmiCalculator from './bmiCalculator';
 
 const app = express();
-const port = 3003;
+const port = 3002;
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+  res.status(200).send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  try {
+    const bmiArgs = bmiCalculator.parseArguments(req.query);
+    const bmiResult = bmiCalculator.calculator(bmiArgs);
+    res.status(200).send({ ...bmiArgs, bmi: bmiResult });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
