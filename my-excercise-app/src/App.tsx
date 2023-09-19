1985392009f82c5742645a315148032bbaa17267
d1r0l <xd1r0lx@gmail.com>
2023-09-19 15:06:14 +0400

    completed ex 9.15

--- my-excercise-app/src/App.tsx
+++ my-excercise-app/src/App.tsx
@@ -1,30 +1,93 @@
-interface CourseParts {
+interface CoursePartBase {
   name: string
   exerciseCount: number
 }
 
+interface CoursePartDescription extends CoursePartBase {
+  description: string
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: 'basic'
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number
+  kind: 'group'
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string
+  kind: 'background'
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: Array<string>
+  kind: 'special'
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial
+
 interface ContentProps {
-  courseParts: CourseParts[]
+  courseParts: CoursePart[]
 }
 
 interface HeaderProps {
   courseName: string
 }
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  )
+}
+
 const App = () => {
   const courseName = 'Half Stack application development'
-  const courseParts = [
+
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is an awesome course part',
+      kind: 'basic'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: 'group'
+    },
+    {
+      name: 'Basics of type Narrowing',
+      exerciseCount: 7,
+      description: 'How to go from unknown to string',
+      kind: 'basic'
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: 'Confusing description',
+      backgroundMaterial:
+        'https://type-level-typescript.com/template-literal-types',
+      kind: 'background'
+    },
+    {
+      name: 'TypeScript in frontend',
+      exerciseCount: 10,
+      description: 'a hard part',
+      kind: 'basic'
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      kind: 'special'
     }
   ]
 
@@ -38,10 +101,60 @@ const App = () => {
         {courseParts.map((part, index) => (
           <p key={index}>
             {part.name} {part.exerciseCount}
+            <Part part={part} />
           </p>
         ))}
       </>
     )
+  }
+
+  const Part = ({ part }: { part: CoursePart }): React.JSX.Element => {
+    switch (part.kind) {
+      case 'basic':
+        return (
+          <>
+            <br />
+            <i>{part.description}</i>
+          </>
+        )
+      case 'group':
+        return (
+          <>
+            <br />
+            {'project excercises: '}
+            {part.groupProjectCount}
+          </>
+        )
+      case 'background':
+        return (
+          <>
+            <br />
+            <i>{part.description}</i>
+            <br />
+            {'submit to: '}
+            {part.backgroundMaterial}
+          </>
+        )
+      case 'special':
+        return (
+          <>
+            <br />
+            <i>{part.description}</i>
+            <br />
+            {'required skills: '}
+            {part.requirements.map(req => {
+              return (
+                <>
+                  {req}
+                  {req === part.requirements.at(-1) ? null : ', '}
+                </>
+              )
+            })}
+          </>
+        )
+      default:
+        return assertNever(part)
+    }
   }
 
   const Total = ({ courseParts }: ContentProps): React.JSX.Element => {
