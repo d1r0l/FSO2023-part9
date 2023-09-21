7d63ba63aa94bb8369eae85b45cab2e11696303e
d1r0l <xd1r0lx@gmail.com>
2023-09-22 00:07:16 +0400

    Gender component fix

--- patientor/patientor_frontend/src/components/PatientPage/GenderIcon.tsx
+++ patientor/patientor_frontend/src/components/PatientPage/GenderIcon.tsx
@@ -1,13 +1,16 @@
 import { Male, Female, Transgender } from '@mui/icons-material'
+import { Gender } from '../../types'
 
-const GenderIcon = ({ gender }: { gender: string }) => {
+const GenderIcon = ({ gender }: { gender: Gender }) => {
   switch (gender) {
     case 'male':
       return <Male />
     case 'female':
       return <Female />
-    default:
+    case 'other':
       return <Transgender />
+    default:
+      return null
   }
 }
 
