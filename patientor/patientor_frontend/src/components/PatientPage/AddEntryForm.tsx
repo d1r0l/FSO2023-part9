ff5281bfecc6d0db16ca49263d9ff4a678cc9ce2
d1r0l <xd1r0lx@gmail.com>
2023-09-25 13:52:54 +0400

    completed ex 9.28

--- patientor/patientor_frontend/src/components/PatientPage/AddEntryForm.tsx
+++ patientor/patientor_frontend/src/components/PatientPage/AddEntryForm.tsx
@@ -14,6 +14,74 @@ import {
 import patientService from '../../services/patients'
 import { AxiosError } from 'axios'
 
+const StyledTextField = ({
+  required,
+  label,
+  value,
+  setValue
+}: {
+  required: boolean
+  label: string
+  value: string
+  setValue: React.Dispatch<React.SetStateAction<string>>
+}) => {
+  return (
+    <TextField
+      required={required}
+      label={label}
+      variant='standard'
+      sx={{ my: 1 }}
+      value={value}
+      onChange={e => setValue(e.target.value)}
+    />
+  )
+}
+
+const StyledAutocomplete = ({
+  label,
+  options,
+  value,
+  setValue
+}: {
+  label: string
+  options: string[]
+  value: string
+  setValue: React.Dispatch<React.SetStateAction<string>>
+}) => {
+  return (
+    <Autocomplete
+      disablePortal
+      options={options}
+      sx={{ my: 1 }}
+      value={value}
+      onChange={(_, value: string) => setValue(value)}
+      renderInput={params => (
+        <TextField {...params} label={label} variant='standard' />
+      )}
+      disableClearable
+    />
+  )
+}
+
+const StyledSwitch = ({
+  label,
+  value,
+  setValue
+}: {
+  label: string
+  value: boolean
+  setValue: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
+  return (
+    <FormControlLabel
+      control={
+        <Switch checked={value} onChange={e => setValue(e.target.checked)} />
+      }
+      label={label}
+    />
+  )
+}
+
 const AddEntryForm = ({
   patient,
   setPatient
@@ -21,32 +89,40 @@ const AddEntryForm = ({
   patient: Patient
   setPatient: React.Dispatch<React.SetStateAction<Patient | undefined>>
 }) => {
+  // Base entry values
+  const entryTypes = ['HealthCheck', 'OccupationalHealthcare', 'Hospital']
   const [type, setType] = useState<string>('HealthCheck')
   const [description, setDescription] = useState<string>('')
   const [date, setDate] = useState<string>('')
   const [specialist, setSpecialist] = useState<string>('')
   const [diagnosisCodes, setDiagnosisCodes] = useState<string>('')
 
+  // HealthCheck entry values
   const [healthCheckRating, setHealthCheckRating] = useState<string>('Healthy')
-  const [employerName, setEmployerName] = useState<string>('')
-  const [sickLeave, setSickLeave] = useState<boolean>(false)
-  const [sickLeaveStartDate, setSickLeaveStartDate] = useState<string>('')
-  const [sickLeaveEndDate, setSickLeaveEndDate] = useState<string>('')
-
-  const [timer, setTimer] = useState<NodeJS.Timeout>()
-  const [showAlert, setShowAlert] = useState<boolean>(false)
-  const [alertText, setAlertText] = useState<string>('')
-  const [alertSeverity, setAlertSeverity] = useState<
-    'success' | 'info' | 'warning' | 'error'
-  >('success')
-
-  const entryTypes = ['HealthCheck', 'OccupationalHealthcare', 'Hospital']
   const healthCheckRatingTypes = [
     'Healthy',
     'Low risk',
     'High risk',
     'Critical risk'
   ]
+
+  // OccupationalHealthcare entry values
+  const [employerName, setEmployerName] = useState<string>('')
+  const [sickLeave, setSickLeave] = useState<boolean>(false)
+  const [sickLeaveStartDate, setSickLeaveStartDate] = useState<string>('')
+  const [sickLeaveEndDate, setSickLeaveEndDate] = useState<string>('')
+
+  // Hospital entry values
+  const [dischargeDate, setDischargeDate] = useState<string>('')
+  const [dischargeCriteria, setDischargeCriteria] = useState<string>('')
+
+  // Alert values
+  const [timer, setTimer] = useState<NodeJS.Timeout>()
+  const [showAlert, setShowAlert] = useState<boolean>(false)
+  const [alertText, setAlertText] = useState<string>('')
+  const [alertSeverity, setAlertSeverity] = useState<
+    'success' | 'info' | 'warning' | 'error'
+  >('success')
 
   const createAlert = (
     text: string,
@@ -65,26 +141,67 @@ const AddEntryForm = ({
   }
 
   const clearFields = () => {
-    setType('HealthCheck')
     setDescription('')
     setDate('')
     setSpecialist('')
-    setHealthCheckRating('Healthy')
     setDiagnosisCodes('')
+    setHealthCheckRating('Healthy')
+    setEmployerName('')
+    setSickLeave(false)
+    setSickLeaveStartDate('')
+    setSickLeaveEndDate('')
+    setDischargeDate('')
+    setDischargeCriteria('')
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    const newEntry = {
-      type,
-      description,
-      date,
-      specialist,
-      healthCheckRating,
-      diagnosisCodes
+
+    const createEntry = () => {
+      const baseValues = {
+        type,
+        description,
+        date,
+        specialist,
+        diagnosisCodes
+      }
+      switch (type) {
+        case 'HealthCheck':
+          return {
+            ...baseValues,
+            healthCheckRating
+          }
+        case 'OccupationalHealthcare':
+          if (sickLeave)
+            return {
+              ...baseValues,
+              employerName,
+              sickLeave: {
+                startDate: sickLeaveStartDate,
+                endDate: sickLeaveEndDate
+              }
+            }
+          else
+            return {
+              ...baseValues,
+              employerName
+            }
+        case 'Hospital':
+          return {
+            ...baseValues,
+            discharge: {
+              date: dischargeDate,
+              criteria: dischargeCriteria
+            }
+          }
+      }
     }
+
     try {
-      const addedEntry = await patientService.createEntry(patient.id, newEntry)
+      const addedEntry = await patientService.createEntry(
+        patient.id,
+        createEntry()
+      )
       const updatedPatient = {
         ...patient,
         entries: patient.entries.concat(addedEntry)
@@ -103,132 +220,6 @@ const AddEntryForm = ({
   const handleReset = (e: React.FormEvent) => {
     e.preventDefault()
     clearFields()
-  }
-
-  const BaseFields = (): React.JSX.Element => {
-    return (
-      <>
-        <Typography variant='h6' gutterBottom>
-          New entry:
-        </Typography>
-        <Autocomplete
-          disablePortal
-          options={entryTypes}
-          value={type}
-          onChange={(_, value: string) => setType(value)}
-          renderInput={params => (
-            <TextField {...params} label={'Entry type'} variant='standard' />
-          )}
-          disableClearable
-        />
-        <TextField
-          label='Description'
-          required
-          variant='standard'
-          sx={{ my: 1 }}
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-        />
-        <TextField
-          label='Date'
-          required
-          variant='standard'
-          sx={{ my: 1 }}
-          value={date}
-          onChange={e => setDate(e.target.value)}
-        />
-        <TextField
-          label='Specialist'
-          required
-          variant='standard'
-          sx={{ my: 1 }}
-          value={specialist}
-          onChange={e => setSpecialist(e.target.value)}
-        />
-        <TextField
-          label='Diagnosis Codes'
-          variant='standard'
-          sx={{ my: 1 }}
-          value={diagnosisCodes}
-          onChange={e => setDiagnosisCodes(e.target.value)}
-        />
-      </>
-    )
-  }
-
-  const OptionalFields = (): React.JSX.Element => {
-    switch (type) {
-      case 'HealthCheck':
-        return (
-          <>
-            <Autocomplete
-              disablePortal
-              options={healthCheckRatingTypes}
-              sx={{ my: 1 }}
-              value={healthCheckRating}
-              onChange={(_, value: string) => setHealthCheckRating(value)}
-              renderInput={params => (
-                <TextField
-                  {...params}
-                  label={'Healthcheck Rating'}
-                  variant='standard'
-                />
-              )}
-              disableClearable
-            />
-          </>
-        )
-      case 'OccupationalHealthcare':
-        return (
-          <>
-            <TextField
-              label='Employer name'
-              required
-              variant='standard'
-              sx={{ my: 1 }}
-              value={employerName}
-              onChange={e => setEmployerName(e.target.value)}
-            />
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={sickLeave}
-                  onChange={e => setSickLeave(e.target.checked)}
-                />
-              }
-              label='Add sick leave'
-            />
-            {sickLeave && (
-              <>
-                <TextField
-                  label='Sick leave start date'
-                  required={sickLeave}
-                  variant='standard'
-                  sx={{ my: 1 }}
-                  value={sickLeaveStartDate}
-                  onChange={e => setSickLeaveStartDate(e.target.value)}
-                />
-                <TextField
-                  label='Sick leave end date'
-                  required={sickLeave}
-                  variant='standard'
-                  sx={{ my: 1 }}
-                  value={sickLeaveEndDate}
-                  onChange={e => setSickLeaveEndDate(e.target.value)}
-                />
-              </>
-            )}
-          </>
-        )
-      case 'Hospital':
-        return (
-          <>
-            <p>something</p>
-          </>
-        )
-      default:
-        return <div />
-    }
   }
 
   return (
@@ -255,8 +246,96 @@ const AddEntryForm = ({
           flexDirection: 'column'
         }}
       >
-        <BaseFields />
-        <OptionalFields />
+        <Typography variant='h6' gutterBottom>
+          New entry:
+        </Typography>
+        <StyledAutocomplete
+          label='Entry type'
+          options={entryTypes}
+          value={type}
+          setValue={setType}
+        />
+        <StyledTextField
+          required
+          label='Description'
+          value={description}
+          setValue={setDescription}
+        />
+        <StyledTextField
+          required
+          label='Date'
+          value={date}
+          setValue={setDate}
+        />
+        <StyledTextField
+          required
+          label='Specialist'
+          value={specialist}
+          setValue={setSpecialist}
+        />
+        <StyledTextField
+          required
+          label='Diagnosis Codes'
+          value={diagnosisCodes}
+          setValue={setDiagnosisCodes}
+        />
+        {type === 'HealthCheck' && (
+          <>
+            <StyledAutocomplete
+              label='Healthcheck Rating'
+              options={healthCheckRatingTypes}
+              value={healthCheckRating}
+              setValue={setHealthCheckRating}
+            />
+          </>
+        )}
+        {type === 'OccupationalHealthcare' && (
+          <>
+            <StyledTextField
+              required
+              label='Employer name'
+              value={employerName}
+              setValue={setEmployerName}
+            />
+            <StyledSwitch
+              label='Sick leave'
+              value={sickLeave}
+              setValue={setSickLeave}
+            />
+            {sickLeave && (
+              <>
+                <StyledTextField
+                  required={sickLeave}
+                  label='Sick leave start date'
+                  value={sickLeaveStartDate}
+                  setValue={setSickLeaveStartDate}
+                />
+                <StyledTextField
+                  required={sickLeave}
+                  label='Sick leave end date'
+                  value={sickLeaveEndDate}
+                  setValue={setSickLeaveEndDate}
+                />
+              </>
+            )}
+          </>
+        )}
+        {type === 'Hospital' && (
+          <>
+            <StyledTextField
+              required
+              label='Discharge date'
+              value={dischargeDate}
+              setValue={setDischargeDate}
+            />
+            <StyledTextField
+              required
+              label='Discharge criteria'
+              value={dischargeCriteria}
+              setValue={setDischargeCriteria}
+            />
+          </>
+        )}
         <Box sx={{ mt: 2, display: 'flex', justifyContent: 'space-between' }}>
           <Button variant='contained' type='reset' color='error'>
             Reset
